f25bac08f89c93183c5fc2c322c8bb17e0cff762
Kavitha Balakrishnan <kavitha1995@gmail.com>
2022-03-23 17:35:09 +0530

    update permission

--- devit_customization/public/js/role.js
+++ devit_customization/public/js/role.js
@@ -11,13 +11,14 @@ var roleMapping = class CustomRoleMapping {
         this.wrapper = $(parent).empty();
         this.role = role;
         this.modules_list = [];
+        this.perm_list = ['Select', 'Read', 'Write', 'Create', 'Delete', 'Submit', 'Amend', 'Print', 'Email', 'Report', 'Import', 'Export', 'Share', 'Set User Permissions'];
 
         this.make_html();
         this.setup();
     }
     make_html() {
         let html = $(`
-            <div>
+            <div id="role-div">
                 <table class="table table-bordered">
                     <thead>
                         <tr>
@@ -25,29 +26,32 @@ var roleMapping = class CustomRoleMapping {
                             <th rowspan="2" style="vertical-align: middle;">${__("Group")}</th>
                             <th rowspan="2" style="vertical-align: middle;">${__("Document Type")}</th>
                             <th rowspan="2" style="vertical-align: middle;">${__("Level")}</th>
-                            <th colspan="10" style="text-align: center;">${__("Permissions")}</th>
+                            <th colspan="${this.perm_list.length}" style="text-align: center;">${__("Permissions")}</th>
                         </tr>
-                        <tr>
-                            <th>${__("Select")}</th>
-                            <th>${__("Read")}</th>
-                            <th>${__("Write")}</th>
-                            <th>${__("Create")}</th>
-                            <th>${__("Delete")}</th>
-                            <th>${__("Submit")}</th>
-                            <th>${__("Amend")}</th>
-                            <th>${__("Print")}</th>
-                            <th>${__("Report")}</th>
-                            <th>${__("Export")}</th>
-                        </tr>
+                        <tr class="permission"></tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td colspan="14" style="text-align: center;">${__("No records found!")}</td>
+                            <td colspan="${this.perm_list.length + 4}" style="text-align: center;">${__("No records found!")}</td>
                         </tr>
                     </tbody>
                 </table>
             </div>
+            <style>
+                #role-div .table th, #role-div .table td {
+                    padding: 8px 5px;
+                }
+                #role-div .checkbox {
+                    text-align: center;
+                }
+                #role-div tr[data-parent-type="module"] {
+                    display: none;
+                }
+            </style>
         `).appendTo(this.wrapper);
+        this.perm_list.map(p => {
+            this.wrapper.find('thead .permission').append(`<th>${__(p)}</th>`);
+        })
     }
     setup() {
         this.get_doctype_data()
@@ -68,9 +72,10 @@ var roleMapping = class CustomRoleMapping {
         })
     }
     map_html() {
+        // let me = this;
         if(this.modules_list.length > 0) {
             this.wrapper.find('tbody').html('');
-            this.expanded_id = 0;
+            // this.active_module;
             this.modules_list.map((module, i) => {
                 let row = $(`<tr data-idx="${i}" data-mod="${module.name}">
                     <td>
@@ -79,26 +84,88 @@ var roleMapping = class CustomRoleMapping {
                                 <use class="" href="#icon-down"></use>
                             </svg>
                             <svg class="icon icon-md icon-up" style="display: none;cursor: pointer;">
-                                <use class="" href="#icon-down"></use>
+                                <use class="" href="#icon-up-line"></use>
                             </svg>
                         </div>
                     </td>
                     <td>${__(module.name)}</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td></td><td></td>
                 </tr>`);
+                row.find('.icon-down').click(() => {
+                    // me.active_module = module.name;
+                    // this.wrapper.find('tbody tr[data-parent-type="module"]').slideUp();
+                    this.wrapper.find('tbody tr[data-parent="' + module.name + '"]').slideDown();
+                    row.find('.icon-down').hide();
+                    row.find('.icon-up').show();
+                });
+                row.find('.icon-up').click(() => {
+                    this.wrapper.find('tbody tr[data-parent="' + module.name + '"]').slideUp();
+                    row.find('.icon-up').hide();
+                    row.find('.icon-down').show();
+                })
+                this.perm_list.map(p => {
+                    row.append(`<td></td>`);
+                })
                 this.wrapper.find('tbody').append(row);
+                module.doctypes.map(dt => {
+                    if(!dt.permissions || dt.permissions.length == 0) {
+                        let dt_row = $(`<tr data-parent="${module.name}" data-idx="0" data-dt="${dt.name}" data-parent-type="module">
+                            <td></td><td></td>
+                            <td>${__(dt.name)}</td>
+                            <td class="text-center">0</td>
+                        </tr>`);
+                        dt_row = this.setup_checkboxes(dt_row, dt.name);
+                        this.wrapper.find('tbody').append(dt_row);
+                    }
+                    dt.permissions.map((perm, k) => {
+                        let dt_row = $(`<tr data-parent="${module.name}" data-idx="${k}" data-dt="${dt.name}" data-parent-type="module">
+                            <td></td><td></td>
+                            <td>${__(dt.name)}</td>
+                            <td class="text-center">${perm.permlevel}</td>
+                        </tr>`);
+                        dt_row = this.setup_checkboxes(dt_row, dt.name, perm);
+                        this.wrapper.find('tbody').append(dt_row);
+                    });
+                });
             })
         }
+    }
+    setup_checkboxes(row_html, dt, permissions) {
+        this.perm_list.map(p => {
+            let perm = p.toLowerCase().replace(/ /g, '_')
+            let ch = $(`<td>
+                <div class="checkbox">
+                    <label>
+                        <span class="input-area"><input type="checkbox" autocomplete="off" class="input-with-feedback" data-fieldtype="Check" data-fieldname="${perm}" placeholder="" data-doctype="Role"></span>
+                        <span class="disp-area" style="display: none;"><input type="checkbox" disabled="" class="disabled-selected"></span>
+                    </label>
+                </div>
+            </td>`);
+            let level = 0
+            if(permissions && permissions[perm] == 1)
+                ch.find('input[type="checkbox"][data-fieldname="' + perm + '"]').prop('checked', true);
+            if(permissions)
+                level = permissions.permlevel || 0;
+            ch.find('input[type="checkbox"][data-fieldname="' + perm + '"]').change(() => {
+                let is_checked = ch.find('input[type="checkbox"][data-fieldname="' + perm + '"]').is(':checked');
+                this.update_permission(dt, level, perm, (is_checked ? 1 : 0));
+            })
+            row_html.append(ch);
+        });
+        return row_html;
+    }
+    update_permission(dt, plevel, ptype, value) {
+        let me = this;
+        frappe.call({
+            method: 'devit_customization.utils.update_permission',
+            args: {
+                doctype: dt,
+                role: me.role,
+                permlevel: plevel,
+                ptype: ptype,
+                value: value
+            },
+            freeze: true
+        })
     }
 }
--- devit_customization/utils.py
+++ devit_customization/utils.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from frappe.permissions import get_all_perms, get_linked_doctypes
+from frappe.permissions import get_all_perms, get_linked_doctypes, update_permission_property
 
 def validate_item(doc, method=None):
     check_records = frappe.get_list('Item', filters=[["cwg1", "=", "%s"%(doc.cwg1)], ['name', '!=', str(doc.name)]])
@@ -24,8 +24,8 @@ def get_doctype_data(role):
         doctypes = frappe.get_all('DocType', order_by='name', 
             filters={'istable': 0, 'name': ('not in', ','.join(not_allowed_in_permission_manager)), 'module': module.name},
             or_filters={"ifnull(restrict_to_domain, '')": "", "restrict_to_domain": ("in", active_domains), 'module': module.name})
-        # for dt in doctypes:
-        #     dt.permissions = get_permissions(dt.name, role)
+        for dt in doctypes:
+            dt.permissions = get_permissions(dt.name, role)
         module.doctypes = doctypes
     return modules_list
 
@@ -55,3 +55,20 @@ def get_permissions(doctype=None, role=None):
             d.in_create = meta.in_create
 
     return out
+
+@frappe.whitelist()
+def update_permission(doctype, role, permlevel, ptype, value=None):
+	"""Update role permission params
+
+	Args:
+		doctype (str): Name of the DocType to update params for
+		role (str): Role to be updated for, eg "Website Manager".
+		permlevel (int): perm level the provided rule applies to
+		ptype (str): permission type, example "read", "delete", etc.
+		value (None, optional): value for ptype, None indicates False
+
+	Returns:
+		str: Refresh flag is permission is updated successfully
+	"""
+	out = update_permission_property(doctype, role, permlevel, ptype, value)
+	return 'refresh' if out else None
