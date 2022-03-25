d8b3d91d2b5521419c8d1e8ec5a9b077a6869a0c
Hardik Zinzuvadiya <hardik.zinzuvadiya@devitpl.com>
2022-03-25 13:06:24 +0530

    [IMP] #Article name logic added.

--- devit_customization/public/js/article.js
+++ devit_customization/public/js/article.js
@@ -1,8 +1,22 @@
 frappe.ui.form.on('Item', {
     refresh: function(frm) {
+
+        if (frappe.boot.lang == 'de'){
+            frm.page.set_title(frm.doc.article_name_german)
+        }
+        if (frappe.boot.lang == 'it'){
+            frm.page.set_title(frm.doc.article_name_italien)
+        }
+        else{
+            frm.page.set_title(frm.doc.article_name_english)
+        }
         frm.trigger('set_mandatory_fields');
-        console.log("\n========frm.doc.article_name_german>>", frm.doc)
-		frm.toggle_display('netto_weight_in_kg', frm.doc. == "test");
+
+		frm.toggle_display(['netto_weight_in_kg'], frm.doc.article_type == "test");
+    },
+
+    article_type(frm){
+        frm.toggle_display(['netto_weight_in_kg'], frm.doc.article_type == "test");
     },
 
     set_mandatory_fields(frm) {
