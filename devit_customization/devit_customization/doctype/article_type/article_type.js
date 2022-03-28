27a26ff1256e1052ceefbb68300d1143971c1fcb
Hardik Zinzuvadiya <hardik.zinzuvadiya@devitpl.com>
2022-03-28 15:01:17 +0530

    [IMP] #Data File Added.

--- devit_customization/devit_customization/doctype/article_type/article_type.js
+++ devit_customization/devit_customization/doctype/article_type/article_type.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Article Type', {
-	// refresh: function(frm) {
-
-	// }
+	 refresh: function(frm) {
+        if (frappe.boot.lang == 'de'){
+            frm.page.set_title(frm.doc.article_type_german)
+        }
+        if (frappe.boot.lang == 'it'){
+            frm.page.set_title(frm.doc.article_type_italian)
+        }
+        else{
+            frm.page.set_title(frm.doc.article_type_english)
+        }
+	 }
 });
--- devit_customization/install.py
+++ devit_customization/install.py
@@ -1,4 +1,4 @@
-# import frappe
+import frappe, json
 from frappe import _
 from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
@@ -23,3 +23,17 @@ def after_install():
     # insert custom field
     for field in field_list:
         create_custom_field(dt, field)
+
+    add_article_type_data()
+
+def add_article_type_data():
+    article_datas = json.loads(
+        open(frappe.get_app_path("devit_customization", "data", "article_type_data.json")).read())
+    for d in article_datas:
+        if not frappe.db.exists('Article Type', _(d.get("article_type_english"))):
+            type_data_inserted = frappe.get_doc({
+                "doctype": "Article Type",
+                "article_type_english": _(d.get("article_type_english")),
+                "article_type_german": _(d.get("article_type_german")),
+            }).db_insert()
+
