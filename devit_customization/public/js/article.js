160a50f37675fce89aa4527bb20ba468b892ff83
dux-prajapati <prajapatidux@gmail.com>
2022-03-28 14:54:52 +0530

    [ONCHANGE] display fields according to article type

--- devit_customization/public/js/article.js
+++ devit_customization/public/js/article.js
@@ -12,55 +12,21 @@ frappe.ui.form.on('Item', {
         }
         frm.trigger('set_mandatory_fields');
 
-		frm.toggle_display(['item_group'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good" || frm.doc.article_type == "Raw material" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['discount_group'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-        frm.toggle_display(['netto_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['brutto_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['baked_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['nr_of_items_on_plate'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['minimal_production_amount'], frm.doc.article_type == "Dough");
-		frm.toggle_display(['maximal_production_amount'], frm.doc.article_type == "Dough");
-		frm.toggle_display(['plates_per_trolley'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['amount_storage_unit'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good" || frm.doc.article_type == "Raw material" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['discount_group', 'kg_price', 'preisschild', 'verrechnung', 'package_amount_in_kg', 'vat'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
+        frm.toggle_display(['netto_weight_in_kg', 'brutto_weight_in_kg', 'baked_weight_in_kg', 'nr_of_items_on_plate', 'plates_per_trolley', 'retours', 'retourenartikel', 'empty_retours', 'retourenanteilsberechnung'], frm.doc.article_type == "Finished good");
+		frm.toggle_display(['minimal_production_amount', 'maximal_production_amount'], frm.doc.article_type == "Dough");
 		frm.toggle_display(['unit_measure'], frm.doc.article_type == "Retail good");
-		frm.toggle_display(['kg_price'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['preisschild'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['verrechnung'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['package_amount_in_kg'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['retours'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['retourenartikel'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['empty_retours'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['retourenanteilsberechnung'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['vat'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['purchasing_production'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['production_costs'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['replacement_in_hours'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['purchasing_production', 'production_costs', 'replacement_in_hours'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['average_nutritions_per_slice_', 'minerals'], frm.doc.article_type != "Raw material")
     },
 
     article_type(frm){
-		frm.toggle_display(['item_group'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good" || frm.doc.article_type == "Raw material" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['discount_group'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-        frm.toggle_display(['netto_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['brutto_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['baked_weight_in_kg'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['nr_of_items_on_plate'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['minimal_production_amount'], frm.doc.article_type == "Dough");
-		frm.toggle_display(['maximal_production_amount'], frm.doc.article_type == "Dough");
-		frm.toggle_display(['plates_per_trolley'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['amount_storage_unit'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good" || frm.doc.article_type == "Raw material" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['discount_group', 'kg_price', 'preisschild', 'verrechnung', 'package_amount_in_kg', 'vat'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
+        frm.toggle_display(['netto_weight_in_kg', 'brutto_weight_in_kg', 'baked_weight_in_kg', 'nr_of_items_on_plate', 'plates_per_trolley', 'retours', 'retourenartikel', 'empty_retours', 'retourenanteilsberechnung'], frm.doc.article_type == "Finished good");
+		frm.toggle_display(['minimal_production_amount', 'maximal_production_amount'], frm.doc.article_type == "Dough");
 		frm.toggle_display(['unit_measure'], frm.doc.article_type == "Retail good");
-		frm.toggle_display(['kg_price'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['preisschild'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['verrechnung'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['package_amount_in_kg'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['retours'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['retourenartikel'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['empty_retours'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['retourenanteilsberechnung'], frm.doc.article_type == "Finished good");
-		frm.toggle_display(['vat'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Retail good");
-		frm.toggle_display(['purchasing_production'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['production_costs'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
-		frm.toggle_display(['replacement_in_hours'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['purchasing_production', 'production_costs', 'replacement_in_hours'], frm.doc.article_type == "Finished good" || frm.doc.article_type == "Dough");
+		frm.toggle_display(['average_nutritions_per_slice_', 'minerals'], frm.doc.article_type != "Raw material")
     },
 
     set_mandatory_fields(frm) {
